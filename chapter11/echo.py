6e883596b5e4b5b0d1fc8646c2ce0fe78146f2c2
roy <roy_radio@126.com>
2015-01-14 13:45:50 +0800

    done

---
+++ chapter11/echo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from twisted.internet import protocol, reactor
+
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
--- chapter11/test_echo.py
+++ chapter11/test_echo.py
@@ -12,5 +12,5 @@ class EchoServerTestCase(unittest.TestCase):
         self.transport = proto_helpers.StringTransport()
 
         self.proto.makeConnection(self.transport)
-        self.proto.dateReceived("test\r\n")
+        self.proto.dataReceived("test\r\n")
         self.assertEqual(self.transport.value(), "test\r\n")
